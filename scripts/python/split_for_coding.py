a1899d2f47d12627d1c7ce7d350a025cb954fb11
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-12-10 17:18:49 -0500

    Add final data for coding
    Create final.tsv with articles in order to finalize coding once the typology is ready

--- scripts/python/split_for_coding.py
+++ scripts/python/split_for_coding.py
@@ -36,6 +36,56 @@ def parse_args():
     return args.input, args.output, args.log_level, args.deep_logging
 
 
+def import_data(input_file):
+    """Import data."""
+    return pd.read_json(input_file)
+
+
+def clean_data(df):
+    """Clean data.
+    Remove rows with missing values.
+    Remove removed articles.
+    """
+    df_clean = df.copy()
+    df_clean.dropna(inplace=True)
+    # remove removed articles
+    df_clean = df_clean[df_clean["source"] != "[Removed]"]
+    return df_clean
+
+
+def sample_data(df, n=200):
+    """Sample data.
+    Sample 200 articles.
+
+    Return sampled data and non-sampled data.
+    """
+    df_sampled = df.sample(n=n)
+
+    df_non_sampled = df.copy()
+    df_non_sampled.drop(df_sampled.index, inplace=True)  # pyright: ignore
+
+    return df_sampled, df_non_sampled
+
+
+def log_data(df_sampled, df_non_sampled, logger):
+    """Log data."""
+    logger.info(f"Number of sampled articles: {len(df_sampled)}")
+    logger.info(df_sampled.head())
+    logger.info(f"Number of non-sampled articles: {len(df_non_sampled)}")
+    logger.info(df_non_sampled.head())
+
+
+def create_final_data(df_sampled, df_non_sampled):
+    df_final = df_sampled.copy()
+    # add non-sampled data to end of sampled data
+    df_final._append(df_non_sampled)
+    return df_final
+
+
+def output_data(df, output_file):
+    df.to_csv(output_file, sep="\t", index=False)
+
+
 def main():
     input_file, output_file, log_level, deep_logging = parse_args()
     np.random.seed(42)
@@ -44,20 +94,29 @@ def main():
     logger = logging.getLogger(__name__)
 
     logger.info("Loading data...")
-    df = pd.read_json(input_file)
-    df_clean = df.copy()
-    df_clean.dropna(inplace=True)
 
-    # remove removed articles
-    df_clean = df_clean[df_clean["source"] != "[Removed]"]
-    # randomly sample 200 articles
-    df_sampled = df_clean.sample(n=200)
+    df = import_data(input_file)
 
-    logger.info("Writing data...")
+    logger.info("Cleaning data...")
 
-    logger.info(df_sampled.head())
+    df_clean = clean_data(df)
 
-    df_sampled.to_csv(output_file, sep="\t", index=False)
+    logger.info("Sampling data...")
+
+    df_sampled, df_non_sampled = sample_data(df_clean)
+
+    log_data(df_sampled, df_non_sampled, logger)
+
+    logger.info("Compiling final data...")
+
+    df_final = create_final_data(df_sampled, df_non_sampled)
+
+    logger.info("Outputting data...")
+
+    output_data(df_sampled, output_file)
+
+    final_output_file = output_file.split(".")[0] + "_final.tsv"
+    output_data(df_final, final_output_file)
 
     pass
 
